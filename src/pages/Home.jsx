492d7d0fb472c17ebd8fa056cdd248929608beab
Realitylion <dhanushbaalaji123@gmail.com>
2024-11-06 00:16:38 +0530

    fix: pages order in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,33 +6,13 @@ const Home = () => {
       <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>Homepage</h1>
       <ul style={{ listStyle: "none", padding: "0" }}>
         <li style={{ marginBottom: "10px" }}>
-          <Link to="/login" style={{ color: "#87CEFA", textDecoration: "none" }}>
-            Login
-          </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <Link to="/home1" style={{ color: "#87CEFA", textDecoration: "none" }}>
-            Home1
-          </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <Link to="/workoutplannermain" style={{ color: "#87CEFA", textDecoration: "none" }}>
-            WorkoutPlannermain
-          </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <Link to="/wellnesssessionmain" style={{ color: "#87CEFA", textDecoration: "none" }}>
-            Wellness SessionMain
-          </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
           <Link to="/landing" style={{ color: "#87CEFA", textDecoration: "none" }}>
             Landing
           </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
-          <Link to="/generateworkoutplan" style={{ color: "#87CEFA", textDecoration: "none" }}>
-            GenerateWorkoutPlan
+          <Link to="/login" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            Login
           </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
@@ -41,18 +21,8 @@ const Home = () => {
             </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
-            <Link to="/wellnesssessionsession" style={{ color: "#87CEFA", textDecoration: "none" }}>
-                WellnessSessionSession
-            </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
             <Link to="/forgotpassword" style={{ color: "#87CEFA", textDecoration: "none" }}>
                 ForgotPassword
-            </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-            <Link to="/exercisetracker" style={{ color: "#87CEFA", textDecoration: "none" }}>
-                ExerciseTracker
             </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
@@ -61,23 +31,53 @@ const Home = () => {
             </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
-            <Link to="/changepassword" style={{ color: "#87CEFA", textDecoration: "none" }}>
-                ChangePassword
-            </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-            <Link to="/pagenotfound" style={{ color: "#87CEFA", textDecoration: "none" }}>
-                PageNotFound
-            </Link>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
             <Link to="/completeprofile" style={{ color: "#87CEFA", textDecoration: "none" }}>
                 CompleteProfile
             </Link>
         </li>
         <li style={{ marginBottom: "10px" }}>
+          <Link to="/home1" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            Home1
+          </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
             <Link to="/settings" style={{ color: "#87CEFA", textDecoration: "none" }}>
                 Settings
+            </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+            <Link to="/changepassword" style={{ color: "#87CEFA", textDecoration: "none" }}>
+                ChangePassword
+            </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <Link to="/workoutplannermain" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            WorkoutPlannermain
+          </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <Link to="/generateworkoutplan" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            GenerateWorkoutPlan
+          </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+            <Link to="/exercisetracker" style={{ color: "#87CEFA", textDecoration: "none" }}>
+                ExerciseTracker
+            </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <Link to="/wellnesssessionmain" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            Wellness SessionMain
+          </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+            <Link to="/wellnesssessionsession" style={{ color: "#87CEFA", textDecoration: "none" }}>
+                WellnessSessionSession
+            </Link>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+            <Link to="/pagenotfound" style={{ color: "#87CEFA", textDecoration: "none" }}>
+                PageNotFound
             </Link>
         </li>
       </ul>
