d12a622f30b4178b4b45d622b7e6fc9b9f30d9ec
Gauri <gaurisharma1018@gmail.com>
2024-11-10 00:36:16 +0530

    Updated description text

--- src/pages/MealPlannermain/index.jsx
+++ src/pages/MealPlannermain/index.jsx
@@ -12,18 +12,18 @@ export default function MealPlannermainPage() {
 
     const navigateMealPlan = () => {
         navigate("/generatemealplan");
-    }
+    };
 
     const navigateCalories = () => {
         navigate("/meallog");
-    }
+    };
 
     // Fetch fun facts from the backend when the component mounts
     useEffect(() => {
         fetch("http://localhost:4000/api/getFunFact")
-            .then(response => response.json())
-            .then(data => setFunFacts(data))
-            .catch(error => console.error("Error fetching fun facts:", error));
+            .then((response) => response.json())
+            .then((data) => setFunFacts(data))
+            .catch((error) => console.error("Error fetching fun facts:", error));
     }, []);
 
     return (
@@ -35,41 +35,41 @@ export default function MealPlannermainPage() {
             <div className="w-full h-[100vh]">
                 <NavBar />
                 <div className="flex h-full flex-col items-center gap-4 bg-coolgray-10">
-                    <div className="flex-1 mx-auto mb-1 flex w-full max-w-[1406px] items-center gap-[30px] self-stretch md:flex-col md:px-5">     
+                    <div className="flex-1 mx-auto mb-1 flex w-full max-w-[1406px] items-center gap-[30px] self-stretch md:flex-col md:px-5">
                         <div className="h-[700px] flex-1 md:self-stretch">
                             <div className="rounded-[10px] bg-default-white p-5">
-                            <div className="mt-4 flex items-center md:flex-col">
-                                <div className="flex flex-1 flex-col gap-11 md:self-stretch">
-                                    <MealPlanGenerator buttonClickHandler={navigateMealPlan} />
-                                    <MealPlanGenerator
-                                        buttonClickHandler={navigateCalories}
-                                        titleText="Track your Calories"
-                                        descriptionText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                                        buttonText="Track Calories"
-                                    />
+                                <div className="mt-4 flex items-center md:flex-col">
+                                    <div className="flex flex-1 flex-col gap-11 md:self-stretch">
+                                        <MealPlanGenerator buttonClickHandler={navigateMealPlan} />
+                                        <MealPlanGenerator
+                                            buttonClickHandler={navigateCalories}
+                                            titleText="Track your Calories"
+                                            descriptionText="Tracking calories helps you make mindful choices and supports your health goals."
+                                            buttonText="Track Calories"
+                                        />
+                                    </div>
+                                    <div className="mt-2.5 flex w-[42%] flex-col items-end gap-[72px] px-7 md:w-full md:gap-[54px] sm:gap-9 sm:px-5">
+                                        <div className="flex w-[90%] flex-col items-start gap-[26px] md:w-full">
+                                            <Heading
+                                                size="3"
+                                                as="h1"
+                                                className="font-spacegrotesk text-[30px] font-medium text-color_black-900_01 md:text-[28px] sm:text-[26px]"
+                                            >
+                                                Analytics
+                                            </Heading>
+                                            <Img
+                                                src="images/favourite_foods.png"
+                                                alt="Screenshot 2024"
+                                                className="rounded-[26px] object-cover md:h-auto"
+                                            />
+                                        </div>
+                                        <Img
+                                            src="images/diet_nutrients.png"
+                                            alt="Screenshot2024"
+                                            className="h-[266px] w-[90%] rounded-[26px] object-contain"
+                                        />
+                                    </div>
                                 </div>
-                                <div className="mt-2.5 flex w-[42%] flex-col items-end gap-[72px] px-7 md:w-full md:gap-[54px] sm:gap-9 sm:px-5">
-                                <div className="flex w-[90%] flex-col items-start gap-[26px] md:w-full">
-                                    <Heading
-                                    size="3"
-                                    as="h1"
-                                    className="font-spacegrotesk text-[30px] font-medium text-color_black-900_01 md:text-[28px] sm:text-[26px]"
-                                    >
-                                    Analytics
-                                    </Heading>
-                                    <Img
-                                        src="images/favourite_foods.png"
-                                        alt="Screenshot 2024"
-                                        className="rounded-[26px] object-cover md:h-auto"
-                                    />
-                                </div>
-                                <Img
-                                    src="images/diet_nutrients.png"
-                                    alt="Screenshot2024"
-                                    className="h-[266px] w-[90%] rounded-[26px] object-contain"
-                                />
-                                </div>
-                            </div>
                             </div>
                         </div>
                         <div className="max-h-[700px] w-1/5 md:w-full overflow-auto">
